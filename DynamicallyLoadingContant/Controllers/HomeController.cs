be3696814b26bcd8a1d8647fdf9ada80e02d61f1
Tofael23 <51793134+Tofael23@users.noreply.github.com>
2020-01-28 02:56:09 +0600

    Created Date 09-07-2017

---
+++ DynamicallyLoadingContant/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using DynamicallyLoadingContant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DynamicallyLoadingContant.Controllers
+{
+    public class HomeController : Controller
+    {
+        MVCTutorialEntities db = new MVCTutorialEntities();
+        public ActionResult Index()
+        {
+            return View(db.EmployeeInfoes.ToList());
+        }
+
+        public ActionResult EmployeeInfo(int id)
+        {
+            List<EmployeeInfo> EmpInfo = db.EmployeeInfoes.Where(x => x.EmployeeId == id).ToList();
+            return View(EmpInfo);
+        }
+    }
+}
